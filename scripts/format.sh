7513c87eaf649cbaa2ff641cfeb62c17a6227d6d
Devin Singh <drsingh2518@icloud.com>
2023-04-16 19:45:45 -0400

    Fix matlab formatting

--- scripts/format.sh
+++ scripts/format.sh
@@ -2,20 +2,20 @@
 
 echo "Instructions for $1"
 echo "["
-grep "^Instructions: " $1 | awk '{printf "    %s%s\n", $2, sep; sep=","}'
+grep "^Instructions: " $1 | awk '{printf "    %s\n", $2}'
 echo "]"
 
 echo "Clocks for $1"
 echo "["
-grep "^Clocks: " $1 | awk '{printf "    %s%s\n", $2, sep; sep=","}'
+grep "^Clocks: " $1 | awk '{printf "    %s\n", $2}'
 echo "]"
 
 echo "Speed for $1"
 echo "["
-tail -n 24 $1 | head -n 19 | awk '{printf "    %s%s\n", $2, sep; sep=","}'
+tail -n 24 $1 | head -n 19 | awk '{printf "    %s\n", $2}'
 echo "]"
 
 echo "Benchmark names for $1"
 echo "["
-tail -n 24 $1 | head -n 19 | awk '{printf "    \"%s\"%s\n", $1, sep; sep=","}'
+tail -n 24 $1 | head -n 19 | awk '{printf "    \"%s\"\n", $1}'
 echo "]"
